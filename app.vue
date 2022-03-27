dc92a2b8d7df389a4ebe63ce646a15808315dc60
Илья Элланский <i.ellansky@rabota.ru>
2022-03-27 21:28:45 +0300

    added vue-query

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script lang="ts" setup>
+// import { VueQueryDevTools } from 'vue-query/devtools';
+</script>
+
 <template>
+  <!-- <VueQueryDevTools /> -->
   <main class="py-20 px-10 text-center">
     <NuxtPage />
     <Footer />
@@ -6,5 +11,5 @@
 </template>
 
 <style>
-@import '~/styles/main.css';
+@import "~/styles/main.css";
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,9 @@ export default defineNuxtConfig({
   vueuse: {
     ssrHandlers: true,
   },
+  alias: {
+    // 'vue-query': ['vue-query/esm/'],
+  },
   unocss: {
     uno: true,
     attributify: true,
